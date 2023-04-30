9e121a3971fc0eb7357f0517b2a797cab3d1ac47
Bow Bow <105433790+BOWBOW0609@users.noreply.github.com>
2023-04-30 21:22:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,8 +38,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mafiatamilan6: Asher074@cluster1.baflpif.mongodb.net/?retryWrites=true&w=majority")
 
 
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster1")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Cluster0')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001961559386'))
