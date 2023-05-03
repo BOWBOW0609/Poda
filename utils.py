58acba762f1b300e39dc48c68094fa11f782495c
Bow Bow <105433790+BOWBOW0609@users.noreply.github.com>
2023-05-03 11:36:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -381,7 +381,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://urlshorten.in'
+    url = URL_SHORTENR_WEBSITE
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
