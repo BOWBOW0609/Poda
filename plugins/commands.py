dca2c00e2575b70c6430bc35643e6e2100d6a41c
Bow ! Bow ! <105433790+BOWBOW0609@users.noreply.github.com>
2023-06-01 09:43:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,9 +60,11 @@ async def start(client, message):
 
              InlineKeyboardButton('ABOUT ME 😉', callback_data='about')
 
-           ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
+            ]]
+            reply_markup = InlineKeyboardMarkup(buttons)
+            await message.reply_photo(
+
+       
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
